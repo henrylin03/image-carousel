c5ee4de95caa619e7b65b5687728690277ecb792
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-03 17:41:35 +1000

    select left and right buttons from DOM

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const images = document.querySelector(".images").children;
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
 
 console.log(images);
+
+// todo: if already at the very left, disable button, and vice versa
