d29338cc7638ccad26a366096813e1058003b719
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-06 09:36:42 +1000

    clicking next increments currentIdx, and clicking back decrements currentIdx

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-const imagesViewWindow = document.querySelector(".images-viewer");
+const imagesViewer = document.querySelector(".images-viewer");
 const leftBtn = document.querySelector(".left");
 const rightBtn = document.querySelector(".right");
 
 // for clarity, use utility function to calculate whether it is -100% translateX on .images in css (index 1), -200% translateX for index 2 etc etc
 
 // nb: it might be good to know how many total images there are at any given time
-// console.log(images.length);
+const allImages = imagesViewer.children;
+const imagesCount = allImages.length;
+let currentIdx = 0;
+
+function show(idx) {
+  if (idx < 0 || idx > imagesCount) return;
+  imagesViewer.style.transform = `translateX(-${idx * 100}%)`;
+  console.log(idx);
+}
 
 // displaying the image is as simple as just shifting the transform: translateX
 
-let currentIdx = 0;
-
-function show() {}
-
-function showPrevious() {
-  if (currentIdx === 0) return;
-  currentIdx--;
-  console.log(currentIdx);
-  return;
+function showPrevious(currentImageIndex) {
+  if (currentImageIndex === 0) return;
+  currentImageIndex--;
+  console.log(currentImageIndex);
+  show(currentImageIndex);
 }
 
-function showNext() {
-  if (currentIdx === images.length - 1) return;
-  currentIdx++;
-  console.log(currentIdx);
-  return;
+function showNext(currentImageIndex) {
+  if (currentImageIndex === imagesCount - 1) return;
+  currentImageIndex++;
+  show(currentImageIndex);
 }
 
-leftBtn.addEventListener("mousedown", showPrevious);
-rightBtn.addEventListener("mousedown", showNext);
+leftBtn.addEventListener("mousedown", () => showPrevious(currentIdx));
+rightBtn.addEventListener("mousedown", () => showNext(currentIdx));
+
+show(currentIdx);
 
 // todo: if already at the very left, disable button, and vice versa
