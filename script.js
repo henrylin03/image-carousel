644011bff3ccd8f114c7ebae3878477dffbaa349
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-06-07 06:44:28 +1000

    using left and right button moves the "viewer" left and right in order to change images (#3)
    * add div of images to cycle through
    
    this is what odin project requires. i had a different thought process of making whatever is selected the background image. but, considering the whole purpose is the odin project exercise, i might as well follow along
    
    * resize images
    
    * make alt text actually useful
    
    * increase height of carousel. stretch image for entirety of grid
    
    * adjust size of nav buttons at bottom
    
    * remove background colour. adjust border radius of the images rather than the container
    
    * make border rounding dynamic
    
    * manually adjust positioning of chevrons within button
    
    * hide non-displayed images
    
    * increase height to avoid skewing
    
    * use absolute px size for article. log images to ensure  we're actually selecting all the images
    
    * adjust positioning
    
    * add responsive resizing of navigation buttons at bottom
    
    * increase size of left right buttons
    
    * select left and right buttons from DOM
    
    * adjust sizing
    
    * write empty functions for use
    
    * attach event listeners on the buttons
    
    * initialise variable to track current index of image (in the images array-like array(?)) and ensure that if the index is 0, then do nothing when trying to go to previous image. otherwise, decrement by 1
    
    * if index is already at the end of array's index (zero-iindexed, thus minusing one in condition), then do nothing when trying to move to the next image. otherwise, increment index by 1
    
    * create function to show (empty still). have nall navigation be at the top layer. test using css translateY to move images up
    
    * make all images be side-by-side, rather than top-to-bottom
    
    crops weird - side-by-side, i've tested just using -100%, -200% etc to go through images and they aren't really cropped! i think this is because the images are all roughly the same width...?
    
    * add pseudo code
    
    * call the .images div .images-viewer
    
    this is because it's actually what's viewing the images
    
    * ensure correct selector to restore rounded corners
    
    * clicking next increments currentIdx, and clicking back decrements currentIdx
    
    * using left and right buttons navigates through the images
    
    * move functions
    
    * Revert "move functions"
    
    This reverts commit 380259316d5f2a9215754b337da4967f51824dca.
    
    * remove console log statements and comments
    
    * ensure responsive sizing of nav at bottom
    
    * remove comments

---
+++ script.js
@@ -0,0 +1,32 @@
+const imagesViewer = document.querySelector(".images-viewer");
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+
+const allImages = imagesViewer.children;
+const imagesCount = allImages.length;
+let currentIdx = 0;
+
+function show(idx) {
+  if (idx < 0 || idx > imagesCount) return;
+  imagesViewer.style.transform = `translateX(-${idx * 100}%)`;
+  console.log(idx);
+}
+
+function showPrevious() {
+  if (currentIdx === 0) return;
+  currentIdx--;
+  show(currentIdx);
+}
+
+function showNext() {
+  if (currentIdx === imagesCount - 1) return;
+  currentIdx++;
+  show(currentIdx);
+}
+
+leftBtn.addEventListener("mousedown", showPrevious);
+rightBtn.addEventListener("mousedown", showNext);
+
+show(currentIdx);
+
+// todo: if already at the very left, disable button, and vice versa
