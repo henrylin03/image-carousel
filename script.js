c1630ff0cfaa23e27f237464dc15c6b91300b927
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-07 06:55:57 +1000

    create updateDisplay function that is called instead of calling the two functions under it, which shows the image and updates the nav

--- script.js
+++ script.js
@@ -7,26 +7,37 @@ const allImages = imagesViewer.children;
 const imagesCount = allImages.length;
 let currentIdx = 0;
 
-function showImg(idx) {
-  if (idx < 0 || idx > imagesCount) return;
-  imagesViewer.style.transform = `translateX(-${idx * 100}%)`;
+function updateDisplay(idx) {
+  const showImg = () => {
+    if (idx < 0 || idx > imagesCount) return;
+    imagesViewer.style.transform = `translateX(-${idx * 100}%)`;
+  };
+
+  const updateNav = () => {
+    // clear 'selected' class from all children of nav buttons
+  };
+
+  // run
+  showImg();
+  updateNav();
 }
 
 function showPrevious() {
   if (currentIdx === 0) return;
   currentIdx--;
-  showImg(currentIdx);
+  updateDisplay(currentIdx);
 }
 
 function showNext() {
   if (currentIdx === imagesCount - 1) return;
   currentIdx++;
-  showImg(currentIdx);
+  updateDisplay(currentIdx);
 }
 
 leftBtn.addEventListener("mousedown", showPrevious);
 rightBtn.addEventListener("mousedown", showNext);
 
-showImg(currentIdx);
+// initialising
+updateDisplay(currentIdx);
 
 // todo: if already at the very left, disable button, and vice versa
