b3eb7c27d3ed70433a271ff38e44daac526bfbfe
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-07 06:54:19 +1000

    update name of function for clarity
    plan is to put this function under another function so that only that 'wrapper' function is called repeatedly

--- script.js
+++ script.js
@@ -7,27 +7,26 @@ const allImages = imagesViewer.children;
 const imagesCount = allImages.length;
 let currentIdx = 0;
 
-function show(idx) {
+function showImg(idx) {
   if (idx < 0 || idx > imagesCount) return;
   imagesViewer.style.transform = `translateX(-${idx * 100}%)`;
-  console.log(idx);
 }
 
 function showPrevious() {
   if (currentIdx === 0) return;
   currentIdx--;
-  show(currentIdx);
+  showImg(currentIdx);
 }
 
 function showNext() {
   if (currentIdx === imagesCount - 1) return;
   currentIdx++;
-  show(currentIdx);
+  showImg(currentIdx);
 }
 
 leftBtn.addEventListener("mousedown", showPrevious);
 rightBtn.addEventListener("mousedown", showNext);
 
-show(currentIdx);
+showImg(currentIdx);
 
 // todo: if already at the very left, disable button, and vice versa
